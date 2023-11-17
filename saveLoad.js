dab621c9d3dd330d4cadba464d810bf49ce3c4d0
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2023-11-17 06:21:52 +0500

    Update saveLoad.js

--- saveLoad.js
+++ saveLoad.js
@@ -1,4 +1,6 @@
 // saveLoad.js
+
+// Объявляем savedUpgrades как глобальную переменную (если это необходимо)
 let savedUpgrades = [
     { cost: 20, level: 0, clickIncrease: 1, multiplier: 2, opened: false },
     { cost: 200, level: 0, clickIncrease: 2, multiplier: 3, opened: false },
@@ -6,21 +8,24 @@ let savedUpgrades = [
     { cost: 1000, level: 0, clickIncrease: 10, multiplier: 5, opened: false },
     { cost: 3000, level: 0, clickIncrease: 20, multiplier: 10, opened: false },
     { cost: 5000, level: 0, clickIncrease: 50, multiplier: 20, opened: false },
-    { cost: 100, level: 0, clickIncrease: 0, multiplier: 1, opened: false, autoClick: 1 }
+    { cost: 100, level: 0, clickIncrease: 0, multiplier: 1, opened: false, },
+    { cost: 100, level: 0, clickIncrease: 0, multiplier: 1, opened: false, },
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1, opened: false, }
 ];
 
 function loadGame() {
     clickCount = parseInt(localStorage.getItem("clickCount")) || 0;
     clickValue = parseInt(localStorage.getItem("clickValue")) || 1;
-    const savedUpgrades = JSON.parse(localStorage.getItem("upgrades")) || upgrades;
+    catCount = parseInt(localStorage.getItem("catCount")) || 0;
 
-    // Обновляем данные об улучшениях
+    const savedUpgradesJSON = localStorage.getItem("upgrades");
+    savedUpgrades = savedUpgradesJSON ? JSON.parse(savedUpgradesJSON) : savedUpgrades;
+
     for (let index = 0; index < UPGRADE_COUNT; index++) {
         upgrades[index].level = savedUpgrades[index].level;
         upgrades[index].cost = savedUpgrades[index].cost;
         upgrades[index].opened = savedUpgrades[index].opened;
 
-        // Отображаем или скрываем кнопку в соответствии с сохраненным статусом открытия
         const upgradeContainer = document.querySelector(`.hidden${index + 1}`);
         const upgradeCostSpan = document.getElementById(`upgradeCost${index + 1}`);
 
@@ -30,10 +35,12 @@ function loadGame() {
             upgradeContainer.style.display = "none";
         }
 
-        upgradeCostSpan.innerText = upgrades[index].cost; // Обновляем стоимость улучшения в DOM
+        upgradeCostSpan.innerText = upgrades[index].cost;
     }
 
     document.getElementById("clickCount").innerText = clickCount;
     document.getElementById("clickValue").innerText = clickValue;
-    checkUpgradeAvailability(); // Обновляем доступность улучшений при загрузке
+    document.getElementById("catCount").innerText = catCount;
+
+    checkUpgradeAvailability();
 }
