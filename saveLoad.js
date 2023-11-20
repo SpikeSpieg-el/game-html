d58bc96cf6ce51e80d32f76023bb020444074607
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2023-11-21 03:48:09 +0500

    Update saveLoad.js

--- saveLoad.js
+++ saveLoad.js
@@ -1,33 +1,28 @@
 // saveLoad.js
 
-//1{ cost: 20, level: 0, clickIncrease: 1, multiplier: 2, opened: false },
-//2{ cost: 200, level: 0, clickIncrease: 2, multiplier: 3, opened: false },
-//3{ cost: 500, level: 0, clickIncrease: 5, multiplier: 4, opened: false },
-//4{ cost: 1000, level: 0, clickIncrease: 10, multiplier: 5, opened: false },
-//5{ cost: 3000, level: 0, clickIncrease: 20, multiplier: 10, opened: false },
-//6{ cost: 5000, level: 0, clickIncrease: 50, multiplier: 20, opened: false },
-//7{ cost: 100, level: 0, clickIncrease: 0, multiplier: 1, opened: false, catCountIncrease: 1 },
-//8 амбар { cost: 300, level: 0, clickIncrease: 0, multiplier: 1, opened: false, image: "OIG.3NJJoFBIJGj4Z.png"},
-//9 пшено { cost: 300, level: 0, clickIncrease: 0, multiplier: 1, opened: false, image: "Pole.png"}
 
-// Объявляем savedUpgrades как глобальную переменную (если это необходимо)
-// saveLoad.js
 
-// Объявляем savedUpgrades как глобальную переменную (если это необходимо)
+
 let savedUpgrades = [
-    { cost: 20, level: 0, clickIncrease: 1, multiplier: 2, opened: false },
-    { cost: 200, level: 0, clickIncrease: 2, multiplier: 3, opened: false },
-    { cost: 500, level: 0, clickIncrease: 5, multiplier: 4, opened: false },
+    { cost: 20, level: 0, clickIncrease: 1, multiplier: 1.5, opened: false },
+    { cost: 200, level: 0, clickIncrease: 2, multiplier: 2.4, opened: false },
+    { cost: 500, level: 0, clickIncrease: 5, multiplier: 3.2, opened: false },
     { cost: 1000, level: 0, clickIncrease: 10, multiplier: 5, opened: false },
-    { cost: 3000, level: 0, clickIncrease: 20, multiplier: 7, opened: false },
-    { cost: 5000, level: 0, clickIncrease: 50, multiplier: 9, opened: false },
-    { cost: 100, level: 0, clickIncrease: 0, multiplier: 1, opened: false, catCountIncrease: 1 },
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1, opened: false, image: "OIG.3NJJoFBIJGj4Z.png" },
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1, opened: false, resourceIncrease: 1, image: "Pole.png" },
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1, opened: false, resourceIncrease_wood: 1, image: "forest_pilka.png" }
+    { cost: 3000, level: 0, clickIncrease: 20, multiplier: 6.3, opened: false },
+    { cost: 5000, level: 0, clickIncrease: 50, multiplier: 7, opened: false },
+    { cost: 100, level: 0, clickIncrease: 0, multiplier: 1.1, opened: false, catCountIncrease: 1 },
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, image: "OIG.3NJJoFBIJGj4Z.png"},
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.3, opened: false, resourceIncrease: 1, image: "Pole.png"},
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.4, opened: false, resourceIncrease_wood: 1, image: " forest_pilka.png"},
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_stone: 1, image: "OIG.zBJ2V.jpg" },
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, home: 1, image: " dom1.png"}
+
 ];
 
+
+
 function loadGame() {
+
     clickCount = parseInt(localStorage.getItem("clickCount")) || 0;
     clickValue = parseInt(localStorage.getItem("clickValue")) || 1;
     catCount = parseInt(localStorage.getItem("catCount")) || 0;
@@ -36,8 +31,15 @@ function loadGame() {
     document.getElementById("wheatTotalCount").innerText = wheatTotalCount;
 
     // Загружаем количество дерева
-    woodTotalCount = parseInt(localStorage.getItem("woodTotalCount")) || 0;
-    document.getElementById("woodTotalCount").innerText = woodTotalCount;
+    woodTotalCount = parseInt(localStorage.getItem("woodTotalCount")) || 0; 
+    document.getElementById("woodTotalCount").innerText = woodTotalCount;   
+
+    stoneTotalCount = parseInt(localStorage.getItem("stoneTotalCount")) || 0; 
+    document.getElementById("stoneTotalCount").innerText = stoneTotalCount;
+
+    hoseTotalCount = parseInt(localStorage.getItem("hoseTotalCount")) || 0; 
+    document.getElementById("hoseTotalCount").innerText = hoseTotalCount;
+
 
     const savedUpgradesJSON = localStorage.getItem("upgrades");
     savedUpgrades = savedUpgradesJSON ? JSON.parse(savedUpgradesJSON) : savedUpgrades;
@@ -47,23 +49,24 @@ function loadGame() {
         upgrades[index].cost = savedUpgrades[index].cost;
         upgrades[index].opened = savedUpgrades[index].opened;
 
+    
         const upgradeContainer = document.querySelector(`.hidden${index + 1}`);
         const upgradeCostSpan = document.getElementById(`upgradeCost${index + 1}`);
-
+    
         if (upgrades[index].opened) {
             upgradeContainer.style.display = "block";
-
+    
             // Добавляем изображение в контейнер, если оно было открыто
             const upgradeImageContainer = document.getElementById(`hiddenimg${index + 1}`);
-
+    
             // Проверяем, что upgradeImageContainer существует
             if (upgradeImageContainer) {
                 // Очищаем контейнер перед добавлением новых изображений
                 upgradeImageContainer.innerHTML = "";
-
+    
                 for (let i = 0; i < upgrades[index].level; i++) {
                     const imgElement = document.createElement('img');
-                    imgElement.src = upgrades[index].image;
+                    imgElement.src = upgrades[index].image; // Исправлено: использовать свойство image из массива upgrades
                     imgElement.alt = 'acat';
                     imgElement.classList.add('img-sity');
                     upgradeImageContainer.appendChild(imgElement);
@@ -74,15 +77,19 @@ function loadGame() {
         } else {
             upgradeContainer.style.display = "none";
         }
-
+    
         upgradeCostSpan.innerText = upgrades[index].cost;
     }
 
     document.getElementById("woodTotalCount").innerText = woodTotalCount;
-
+    document.getElementById("stoneTotalCount").innerText = stoneTotalCount;
+    document.getElementById("hoseTotalCount").innerText = hoseTotalCount;
     document.getElementById("clickCount").innerText = clickCount;
     document.getElementById("clickValue").innerText = clickValue;
     document.getElementById("catCount").innerText = catCount;
 
     checkUpgradeAvailability();
+    
 }
+
+
