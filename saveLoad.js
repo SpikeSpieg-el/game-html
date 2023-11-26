afcf96acf9147e3d7837855ad44861d729a6bb7f
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2023-11-26 18:57:34 +0300

    1

--- saveLoad.js
+++ saveLoad.js
@@ -4,20 +4,25 @@
 
 
 let savedUpgrades = [
-    { cost: 20, level: 0, clickIncrease: 1, multiplier: 1.5, opened: false },
+     { cost: 20, level: 0, clickIncrease: 1, multiplier: 1.5, opened: false },
     { cost: 200, level: 0, clickIncrease: 2, multiplier: 2.4, opened: false },
     { cost: 500, level: 0, clickIncrease: 5, multiplier: 3.2, opened: false },
     { cost: 1000, level: 0, clickIncrease: 10, multiplier: 5, opened: false },
     { cost: 3000, level: 0, clickIncrease: 20, multiplier: 6.3, opened: false },
     { cost: 5000, level: 0, clickIncrease: 50, multiplier: 7, opened: false },
-    { cost: 100, level: 0, clickIncrease: 0, multiplier: 1.1, opened: false, catCountIncrease: 1 },
+{ cost: 100, level: 0, clickIncrease: 0, multiplier: 1.1, opened: false, catCountIncrease: 1 },
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, image: "OIG.3NJJoFBIJGj4Z.png"},
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.3, opened: false, resourceIncrease: 1, image: "Pole.png"},
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.4, opened: false, resourceIncrease_wood: 1, image: " forest_pilka.png"},
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_stone: 1, image: "OIG.zBJ2V.png" },
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, home: 1, image: " dom1.png"},
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_metall: 1, image: "OIG.bFY9BU.jpeg" }
-
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, home: 1, image: " dom1.png"}, 
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_metall: 1, image: "OIG.bFY9BU.jpeg" },
+{ cost: 10000, level: 0, clickIncrease: 100, multiplier: 8.5, opened: false },
+    { cost: 20000, level: 0, clickIncrease: 200, multiplier: 10, opened: false },
+    { cost: 50000, level: 0, clickIncrease: 500, multiplier: 12, opened: false },
+    { cost: 100000, level: 0, clickIncrease: 1000, multiplier: 15, opened: false },
+    { cost: 200000, level: 0, clickIncrease: 2000, multiplier: 18, opened: false },
+    { cost: 500000, level: 0, clickIncrease: 5000, multiplier: 22, opened: false }
 ];
 
 
