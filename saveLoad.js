c9ad7587dfc760db01466e159904a2c99fe98b16
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-02-12 23:18:29 +0500

    процентовка улутшений

--- saveLoad.js
+++ saveLoad.js
@@ -1,29 +1,29 @@
 // saveLoad.js
 let savedUpgrades = [
-     { cost: 20, level: 0, clickIncrease: 1, multiplier: 1.5, opened: false },
+    { cost: 20, level: 0, clickIncrease: 1, multiplier: 1.5, opened: false },
     { cost: 200, level: 0, clickIncrease: 2, multiplier: 2.4, opened: false },
     { cost: 500, level: 0, clickIncrease: 5, multiplier: 3.2, opened: false },
     { cost: 1000, level: 0, clickIncrease: 10, multiplier: 5, opened: false },
-    { cost: 3000, level: 0, clickIncrease: 20, multiplier: 6.3, opened: false },
-    { cost: 5000, level: 0, clickIncrease: 50, multiplier: 7, opened: false },
+    { cost: 3000, level: 0, clickIncrease: 20, multiplier: 5.25, opened: false },
+    { cost: 5000, level: 0, clickIncrease: 50, multiplier: 6.10, opened: false },
 { cost: 100, level: 0, clickIncrease: 0, multiplier: 1.1, opened: false, catCountIncrease: 1 },
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, image: "OIG.3NJJoFBIJGj4Z.png"},
-    { cost: 300, level: 0, clickIncrease: 1.5, multiplier: 1.3, opened: false, resourceIncrease: 1, image: "Pole.png"},
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.4, opened: false, resourceIncrease_wood: 1, image: " forest_pilka.png"},
+    { cost: 100, level: 0, clickIncrease: 1.5, multiplier: 1.1, opened: false, resourceIncrease: 1, image: "Pole.png"},
+    { cost: 200, level: 0, clickIncrease: 0, multiplier: 1.4, opened: false, resourceIncrease_wood: 1, image: " forest_pilka.png"},
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_stone: 1, image: "OIG.zBJ2V.png" },
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, home: 1, image: " dom1.png"}, 
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 5.25, opened: false, home: 1, image: " dom1.png"}, 
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_metall: 1, image: "OIG.bFY9BU.jpeg" },
 { cost: 10000, level: 0, clickIncrease: 100, multiplier: 8.5, opened: false },
     { cost: 20000, level: 0, clickIncrease: 200, multiplier: 10, opened: false },
-    { cost: 50000, level: 0, clickIncrease: 500, multiplier: 12, opened: false },
-    { cost: 100000, level: 0, clickIncrease: 1000, multiplier: 15, opened: false },
-    { cost: 200000, level: 0, clickIncrease: 2000, multiplier: 18, opened: false },
-    { cost: 500000, level: 0, clickIncrease: 5000, multiplier: 22, opened: false },
-    { cost: 1000000, level: 0, clickIncrease: 10000, multiplier: 23, opened: false },
-{ cost: 1000, level: 0, clickIncrease: 0, multiplier: 1, opened: false},
-    { cost: 1110, level: 0, clickIncrease: 0, multiplier: 1, opened: false},
-    { cost: 2000, level: 0, clickIncrease: 0, multiplier: 1, opened: false},
-     { cost: 5000000, level: 0, clickIncrease: 100000, multiplier: 26, opened: false }
+    { cost: 50000, level: 0, clickIncrease: 500, multiplier: 10.15, opened: false },
+    { cost: 100000, level: 0, clickIncrease: 1000, multiplier: 10.26, opened: false },
+    { cost: 200000, level: 0, clickIncrease: 2000, multiplier: 10.45, opened: false },
+    { cost: 500000, level: 0, clickIncrease: 5000, multiplier: 10.6, opened: false },
+    { cost: 1000000, level: 0, clickIncrease: 10000, multiplier: 10.85, opened: false },
+{ cost: 1000, level: 0, clickIncrease: 0, multiplier: 1.25, opened: false},
+    { cost: 1110, level: 0, clickIncrease: 0, multiplier: 1.35, opened: false},
+    { cost: 2000, level: 0, clickIncrease: 0, multiplier: 1.45, opened: false},
+    { cost: 5000000, level: 0, clickIncrease: 100000, multiplier: 11, opened: false }
 ];
 
 
@@ -41,7 +41,7 @@ function loadGame() {
     stoneTotalCount = parseInt(localStorage.getItem("stoneTotalCount")) || 0;
     hoseTotalCount = parseInt(localStorage.getItem("hoseTotalCount")) || 0;
     metallTotalCount = parseInt(localStorage.getItem("metallTotalCount")) || 0;
-    goldCount = parseInt(localStorage.getItem("goldCount")) || 20;
+    goldCount = parseInt(localStorage.getItem("goldCount")) || 0;
 
     const savedUpgradesJSON = localStorage.getItem("upgrades");
     savedUpgrades = savedUpgradesJSON ? JSON.parse(savedUpgradesJSON) : savedUpgrades;
--- script.js
+++ script.js
@@ -3,27 +3,27 @@ const upgrades = [
     { cost: 200, level: 0, clickIncrease: 2, multiplier: 2.4, opened: false },
     { cost: 500, level: 0, clickIncrease: 5, multiplier: 3.2, opened: false },
     { cost: 1000, level: 0, clickIncrease: 10, multiplier: 5, opened: false },
-    { cost: 3000, level: 0, clickIncrease: 20, multiplier: 6.3, opened: false },
-    { cost: 5000, level: 0, clickIncrease: 50, multiplier: 7, opened: false },
+    { cost: 3000, level: 0, clickIncrease: 20, multiplier: 5.25, opened: false },
+    { cost: 5000, level: 0, clickIncrease: 50, multiplier: 6.10, opened: false },
 { cost: 100, level: 0, clickIncrease: 0, multiplier: 1.1, opened: false, catCountIncrease: 1 },
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, image: "OIG.3NJJoFBIJGj4Z.png"},
-    { cost: 100, level: 0, clickIncrease: 1.5, multiplier: 1.3, opened: false, resourceIncrease: 1, image: "Pole.png"},
+    { cost: 100, level: 0, clickIncrease: 1.5, multiplier: 1.1, opened: false, resourceIncrease: 1, image: "Pole.png"},
     { cost: 200, level: 0, clickIncrease: 0, multiplier: 1.4, opened: false, resourceIncrease_wood: 1, image: " forest_pilka.png"},
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_stone: 1, image: "OIG.zBJ2V.png" },
-    { cost: 300, level: 0, clickIncrease: 0, multiplier: 8, opened: false, home: 1, image: " dom1.png"}, 
+    { cost: 300, level: 0, clickIncrease: 0, multiplier: 5.25, opened: false, home: 1, image: " dom1.png"}, 
     { cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_metall: 1, image: "OIG.bFY9BU.jpeg" },
 { cost: 10000, level: 0, clickIncrease: 100, multiplier: 8.5, opened: false },
     { cost: 20000, level: 0, clickIncrease: 200, multiplier: 10, opened: false },
-    { cost: 50000, level: 0, clickIncrease: 500, multiplier: 12, opened: false },
-    { cost: 100000, level: 0, clickIncrease: 1000, multiplier: 15, opened: false },
-    { cost: 200000, level: 0, clickIncrease: 2000, multiplier: 18, opened: false },
-    { cost: 500000, level: 0, clickIncrease: 5000, multiplier: 22, opened: false },
-    { cost: 1000000, level: 0, clickIncrease: 10000, multiplier: 23, opened: false },
-{ cost: 1000, level: 0, clickIncrease: 0, multiplier: 1, opened: false},
-    { cost: 1110, level: 0, clickIncrease: 0, multiplier: 1, opened: false},
-    { cost: 2000, level: 0, clickIncrease: 0, multiplier: 1, opened: false},
-    { cost: 5000000, level: 0, clickIncrease: 100000, multiplier: 26, opened: false }
-];//1{ cost: 300, level: 0, clickIncrease: 0, multiplier: 1.8, opened: false, resourceIncrease_stone: 1, image: "OIG.zBJ2V.png" }
+    { cost: 50000, level: 0, clickIncrease: 500, multiplier: 10.15, opened: false },
+    { cost: 100000, level: 0, clickIncrease: 1000, multiplier: 10.26, opened: false },
+    { cost: 200000, level: 0, clickIncrease: 2000, multiplier: 10.45, opened: false },
+    { cost: 500000, level: 0, clickIncrease: 5000, multiplier: 10.6, opened: false },
+    { cost: 1000000, level: 0, clickIncrease: 10000, multiplier: 10.85, opened: false },
+{ cost: 1000, level: 0, clickIncrease: 0, multiplier: 1.25, opened: false},
+    { cost: 1110, level: 0, clickIncrease: 0, multiplier: 1.35, opened: false},
+    { cost: 2000, level: 0, clickIncrease: 0, multiplier: 1.45, opened: false},
+    { cost: 5000000, level: 0, clickIncrease: 100000, multiplier: 11, opened: false }
+];//1{ cost: 300, level: 0, clickIncrease: 0, multiplier: 1.85, opened: false, resourceIncrease_stone: 1, image: "OIG.zBJ2V.png" }
 
 let clickCount = 0; // сколько сейчас
 let clickValue = 1; //сколько за клик
